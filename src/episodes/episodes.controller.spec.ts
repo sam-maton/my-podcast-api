0fa98ca40c5d2dd901738d9f6d6a16f4c3a0e821
Sam Maton <sam.maton@aveva.com>
2025-07-29 11:41:47 +0100

    added pipe

--- src/episodes/episodes.controller.spec.ts
+++ src/episodes/episodes.controller.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { EpisodesController } from './episodes.controller';
+import { ConfigModule } from '../config/config.module';
+import { EpisodesService } from './episodes.service';
 
 describe('EpisodesController', () => {
   let controller: EpisodesController;
+  const mockEpisodesService = {
+    findAll: () => [{ id: '1' }],
+    findFeatured: () => [{ id: '1' }],
+    findOne: () => [{ id: '1' }],
+    create: () => [{ id: '1' }],
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule],
       controllers: [EpisodesController],
+      providers: [{ provide: EpisodesService, useValue: mockEpisodesService }],
     }).compile();
 
     controller = module.get<EpisodesController>(EpisodesController);
--- src/episodes/episodes.controller.ts
+++ src/episodes/episodes.controller.ts
@@ -1,12 +1,33 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { EpisodesService } from './episodes.service';
 import { CreateEpisodeDto } from './create-episode.dto';
+import { ConfigService } from '../config/config.service';
+import { IsPositivePipe } from 'src/is-positive/is-positive.pipe';
 
 @Controller('episodes')
 export class EpisodesController {
-  constructor(private episodesService: EpisodesService) {}
+  constructor(
+    private episodesService: EpisodesService,
+    private configService: ConfigService,
+  ) {}
   @Get()
-  findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc') {
+  findAll(
+    @Query('sort') sort: 'asc' | 'desc' = 'desc',
+    @Query('limit', new DefaultValuePipe(100), ParseIntPipe, IsPositivePipe)
+    limit: number = 10,
+  ) {
+    console.log(limit);
     return this.episodesService.findAll(sort);
   }
 
@@ -17,11 +38,16 @@ export class EpisodesController {
 
   @Get(':id')
   findOneFeatured(@Param('id') id: string) {
-    return this.episodesService.findOne(id);
+    const episode = this.episodesService.findOne(id);
+    if (!episode) {
+      throw new HttpException('Episode not found', HttpStatus.NOT_FOUND);
+    }
+    return episode;
   }
 
   @Post()
   create(@Body() body: CreateEpisodeDto) {
-    return this.episodesService.create(body);
+    const newEpisode = this.episodesService.create(body);
+    return newEpisode;
   }
 }
